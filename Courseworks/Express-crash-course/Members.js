f708d5afdf1b4a8d5562b63424e0c00d886ddf0e
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2021-06-30 16:03:31 +0300

    Express crash course end

---
+++ Courseworks/Express-crash-course/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Levi Ackerman',
+        email: 'levi@gmail.com',
+        status: 'Active'
+    },
+    {
+        id:2,
+        name: 'Armin Arlerto',
+        email: 'Arler@gmail.com',
+        status: 'Active'
+    },
+    {
+        id: 3,
+        name: 'Erwin Smith',
+        email: 'Erwin@gmail.com',
+        status: 'Inactive'
+    }
+];
+
+module.exports = members;
