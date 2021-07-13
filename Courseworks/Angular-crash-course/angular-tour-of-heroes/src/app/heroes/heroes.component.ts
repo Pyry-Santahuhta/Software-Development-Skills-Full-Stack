3c1c3bfe1ecd66a34aebbab81ecba55ebab2cfaa
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2021-07-13 14:43:53 +0300

    Angular progress

--- Courseworks/Angular-crash-course/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Courseworks/Angular-crash-course/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name:"Tun'Tsoth"
-  };
+ 
+  heroes = HEROES;
+  selectedHero?: Hero;
   constructor() { }
 
   ngOnInit() {
+    console.log('Hello world');
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
   }
 }
---
+++ Courseworks/Angular-crash-course/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wacoma' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Joosua' },
+  { id: 14, name: "Ki'mi" },
+  { id: 15, name: 'Zuntah' },
+  { id: 16, name: 'Glueman' },
+  { id: 17, name: 'Kulemak' },
+  { id: 18, name: 'The egg' },
+  { id: 19, name: 'Lump of moat' },
+  { id: 20, name: 'Maniel' }
+];
