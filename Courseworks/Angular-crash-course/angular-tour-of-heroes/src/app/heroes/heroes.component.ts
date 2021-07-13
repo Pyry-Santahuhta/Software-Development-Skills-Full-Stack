2c1cd46149b1d0fdc6c1b1165924d26970fcc3bf
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2021-07-13 23:36:32 +0300

    Services angular

--- Courseworks/Angular-crash-course/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Courseworks/Angular-crash-course/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
- 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
-    console.log('Hello world');
+    this.getHeroes();
   }
-  
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log(this.selectedHero);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
